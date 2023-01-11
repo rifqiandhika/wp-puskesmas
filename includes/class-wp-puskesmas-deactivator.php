fdf72fb3d0d9c6253f119abbdc67f285520e76e6
rifqiandhika <rifqismurf50@gmail.com>
2023-01-11 09:15:24 +0700

    0.1

---
+++ includes/class-wp-puskesmas-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/rifqiandhika/
+ * @since      1.0.0
+ *
+ * @package    Wp_Puskesmas
+ * @subpackage Wp_Puskesmas/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Puskesmas
+ * @subpackage Wp_Puskesmas/includes
+ * @author     HellQiii <rifqismurf50@gmail.com>
+ */
+class Wp_Puskesmas_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
