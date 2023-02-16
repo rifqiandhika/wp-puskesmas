7e7f0a24495793c5d38560943f4b9d6ed0634946
Lucky <luckyputraryansyah123@gmail.com>
2023-02-16 15:14:11 +0700

    menambahkan Polygon

--- public/partials/wp-puskesmas-cek-gizi-map.php
+++ public/partials/wp-puskesmas-cek-gizi-map.php
@@ -47,6 +47,7 @@
         window.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
         addMarker(lokasi_aset);
         addPolyline(lokasi_aset);
+        addPolygon(lokasi_aset);
     }
     function addMarker(lokasi) {
         new google.maps.Marker({
@@ -70,5 +71,24 @@
         });
 
   flightPath.setMap(map);
+    }
+    function addPolygon(lokasi){
+        const triangleCoords = [
+            { lat: -7.7359841, lng: 111.4010564 },
+            { lat: -7.7361017, lng: 111.3992391 },
+            { lat: -7.7376992, lng: 111.4014665 },
+            { lat: -7.7359841, lng: 111.4010564 },
+        ];
+        // Construct the polygon.
+        const bermudaTriangle = new google.maps.Polygon({
+            paths: triangleCoords,
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: "#FF0000",
+            fillOpacity: 0.35,
+        });
+
+        bermudaTriangle.setMap(map);
     }   
 </script>
